69620056cc3c8e504dedae69e498c0d1dab25dd9
Cleaner-Discord <36578194+Cleaner-Discord@users.noreply.github.com>
2020-06-10 17:09:28 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = (client) => {
   return () => {
     console.log(`BMO est prêt à fonctionné !`);
-    client.user.setActivity(`${client.settings.get("default").prefix}help`, {type: "PLAYING"});
+    client.user.setActivity(`b!help`, {type: "PLAYING"});
   }
 }
