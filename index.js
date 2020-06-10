54fbbcf3db67dd68c06bd7157930496960aeb19e
Cleaner-Discord <36578194+Cleaner-Discord@users.noreply.github.com>
2020-06-10 15:51:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 
 const client = new Discord.Client();
 const config = require("./config.json");
-// We also need to make sure we're attaching the config to the CLIENT so it's accessible everywhere!
+// Nous devons également nous assurer que nous attachons la configuration au CLIENT pour qu'elle soit accessible partout!
 client.config = config;
 
 fs.readdir("./events/", (err, files) => {
@@ -12,21 +12,45 @@ fs.readdir("./events/", (err, files) => {
   files.forEach(file => {
     const event = require(`./events/${file}`);
     let eventName = file.split(".")[0];
-    client.on(eventName, event.bind(null, client));
+    client.on(eventName, event(client));
   });
 });
 
 client.commands = new Enmap();
 
-fs.readdir("./commands/", (err, files) => {
+fs.readdir("./Commands/", (err, files) => {
   if (err) return console.error(err);
   files.forEach(file => {
     if (!file.endsWith(".js")) return;
-    let props = require(`./commands/${file}`);
+    let props = require(`./Commands/${file}`);
     let commandName = file.split(".")[0];
-    console.log(`Attempting to load command ${commandName}`);
+    console.log(`Tentative de chargement de la commande : ${commandName} \n ------------------------`);
     client.commands.set(commandName, props);
   });
 });
 
+// Important au démarrage du bot!
 client.login(process.env.BOT_TOKEN);
+message.js
+module.exports = (client) => {
+  return (message) => {
+    // Ignore les bots
+    if (message.author.bot) return;
+
+    // Ignore les messages ne commençant pas par le préfix (dans config.json)
+    if (message.content.indexOf(client.config.prefix) !== 0) return;
+    
+    // Notre définition d'argument / nom de commande standard
+    const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+  
+    // Récupère les données dela commande du client.commands Enmap
+    const cmd = client.commands.get(command);
+  
+    // Si la commande n'existe pas, il ignore et ne dis rien
+    if (!cmd) return;
+  
+    // Démarre la commande
+    cmd.run(client, message, args);
+  }
+};
