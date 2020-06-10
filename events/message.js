46082d6081bd41f570f92a8923fff84bea0759b6
Cleaner-Discord <36578194+Cleaner-Discord@users.noreply.github.com>
2020-06-10 15:55:40 +0200

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,20 +1,28 @@
-module.exports = (client, message) => {
-  // Ignore all bots
-  if (message.author.bot) return;
+module.exports = (client) => {
+  return (message) => {
+    // Ignore les bots
+    if (message.author.bot) return;
 
-  // Ignore messages not starting with the prefix (in config.json)
-  if (message.content.indexOf(client.config.prefix) !== 0) return;
-
-  // Our standard argument/command name definition.
-  const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();
-
-  // Grab the command data from the client.commands Enmap
-  const cmd = client.commands.get(command);
-
-  // If that command doesn't exist, silently exit and do nothing
-  if (!cmd) return;
-
-  // Run the command
-  cmd.run(client, message, args);
+    // Ignore les messages ne commençant pas par le préfix (dans config.json)
+    if (message.content.indexOf(client.config.prefix) !== 0) return;
+    
+    // Notre définition d'argument / nom de commande standard
+    const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+  
+    // Récupère les données dela commande du client.commands Enmap
+    const cmd = client.commands.get(command);
+  
+    // Si la commande n'existe pas, il ignore et ne dis rien
+    if (!cmd) return;
+    
+    // Ignorer les DM /!\
+    if(message.channel.type==="dm") {
+    message.channel.send("Impossible d'exécuter une commande en DM ! :BMOuh:")
+    return false;
+  }
+  
+    // Démarre la commande
+    cmd.run(client, message, args);
+  }
 };
