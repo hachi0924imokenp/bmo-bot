b4d462164d5bce5424f1a768a2e45a0126f7fec3
Cleaner-Discord <36578194+Cleaner-Discord@users.noreply.github.com>
2020-06-10 23:13:51 +0200

    some edits

--- commands/test.js
+++ commands/test.js
@@ -3,11 +3,11 @@ const moment = require("moment");
 require("moment-duration-format");
 const duration = moment.duration(client.uptime).format(" J [Jours], H [heures], m [minutes], s [secondes]");
     message.channel.send({embed: {
-    title: "Test",
+    title: "Test :ok_hand:",
       color: 1752220,
       description: `Le bot fonctionne avec un uptime de ${duration}`,
       footer: {
         text:  "BMO" 
       }
-    }})
+    }}).then(() => message.react(':BMOdance:704463259570405447'));
 }
