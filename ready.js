543fcd9b3cc57708302972deb80c444232c16a8a
Cleaner-Discord <36578194+Cleaner-Discord@users.noreply.github.com>
2020-06-12 00:01:16 +0200

    Delete ready.js

--- ready.js
+++
@@ -1,6 +0,0 @@
-module.exports = (client) => {
-  return () => {
-    console.log(`-----------------------\nBMO est prêt à fonctionné !`);
-    client.user.setActivity(`b!help`, {type: "PLAYING"});
-  }
-}
