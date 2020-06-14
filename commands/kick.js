187cedec7a5ccb62456d541a4b5134c2c20d7f81
Cleaner-Discord <36578194+Cleaner-Discord@users.noreply.github.com>
2020-06-14 23:00:38 +0200

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -1,4 +1,4 @@
-exports.run = (client, message, args) => {
+exports.run = (client, message, args, async) => {
 async () => {
     if(!message.member.roles.cache.some(r=>["🛡️ P'tit Modo Test 🛡️", "🌟 Modo T'chat  🌟", "👑 Fondateurs 👑", "👑 Fondateur Principal 👑"]).includes(r.name))
         return message.channel.send(`Désolé <@${message.author.id}, vous n'avez pas la permission à l'utilistion nécessaire de cette commande.`);
