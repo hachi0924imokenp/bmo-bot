bb96547302c6050180e897ed55c8a6fa89f8e2e2
Cleaner-Discord <36578194+Cleaner-Discord@users.noreply.github.com>
2020-06-15 01:27:57 +0200

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -14,7 +14,7 @@ exports.run = async (client, message, args) => {
 
     await member.kick(reason)
         .catch(error => message.channel.send(`Désolé, je ne peux pas kické cette utilisateur à cause de : ${error}`));
-    message.channel.send(`${member.user.tag} à été kické par ${message.author.tag}`);
+    message.channel.send(`${member.user.tag} a été kické par ${message.author.tag}`);
     message.member.send(`Vous avez été kické par ${message.author.tag} ===> ${reason}`);
   
 }
