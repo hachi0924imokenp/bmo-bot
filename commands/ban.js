d692bb8f993e0faa6a406d82ec70ec1cac70a122
Cleaner-Discord <36578194+Cleaner-Discord@users.noreply.github.com>
2020-06-15 18:43:37 +0200

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -32,4 +32,54 @@ exports.run = async (client, message, args) => {
     console.log('Le salon des logs n\'existe pas, et j\'ai essayer de le crée mais je manque de permissions !')
   }
 
+  const banlog = {
+	color: 0x0099ff,
+	title: 'Some title',
+	url: 'https://discord.js.org',
+	author: {
+		name: 'Some name',
+		icon_url: 'https://i.imgur.com/wSTFkRM.png',
+		url: 'https://discord.js.org',
+	},
+	description: 'Some description here',
+	thumbnail: {
+		url: 'https://i.imgur.com/wSTFkRM.png',
+	},
+	fields: [
+		{
+			name: 'Regular field title',
+			value: 'Some value here',
+		},
+		{
+			name: '\u200b',
+			value: '\u200b',
+			inline: false,
+		},
+		{
+			name: 'Inline field title',
+			value: 'Some value here',
+			inline: true,
+		},
+		{
+			name: 'Inline field title',
+			value: 'Some value here',
+			inline: true,
+		},
+		{
+			name: 'Inline field title',
+			value: 'Some value here',
+			inline: true,
+		},
+	],
+	image: {
+		url: 'https://i.imgur.com/wSTFkRM.png',
+	},
+	timestamp: new Date(),
+	footer: {
+		text: 'Some footer text here',
+		icon_url: 'https://i.imgur.com/wSTFkRM.png',
+	},
+};
+
+message.channel.send({ embed: banlog });
 }
