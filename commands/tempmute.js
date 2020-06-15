e4a5bf844059d1e3e8789aa7cac3e87209a53133
Cleaner-Discord <36578194+Cleaner-Discord@users.noreply.github.com>
2020-06-15 05:17:28 +0200

    Update tempmute.js

--- commands/tempmute.js
+++ commands/tempmute.js
@@ -10,7 +10,7 @@ exports.run = async (client, message, args) => {
      if(!muteRole)
          return message.channel.send("Impossible de trouver le rôle 🏝️ No Man\'s Land");
     
-      if(tomute.roles.has(muteRole)) return message.channel.send("That user is already muted!.");
+      if(tomute.roles.cache.has(muteRole)) return message.channel.send("That user is already muted!.");
       tomute.addRole(muteRole);
      
      const MUTE_TIME = 60 * 1000;
