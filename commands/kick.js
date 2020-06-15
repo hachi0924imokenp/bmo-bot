8361189171ece0a36079d7d2b0e6589bd049c9ca
Cleaner-Discord <36578194+Cleaner-Discord@users.noreply.github.com>
2020-06-15 03:52:52 +0200

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -1,10 +1,10 @@
 exports.run = async (client, message, args) => {
     if(!message.member.roles.cache.some(r=>["🐹 Modo T'chat Test 🐹", "🛡️ P'tit Modo 🛡️", "🌟 Modo T'chat  🌟", "👑 Fondateurs 👑", "👑 Fondateur Principal 👑"].includes(r.name)))
-        return message.channel.send(`Désolé`+"<@" + message.author.id + ">, vous n'avez pas la permission à l'utilistion nécessaire de cette commande.`);
+        return message.channel.send(`Désolé <@` + message.author.id + `>, vous n'avez pas la permission à l'utilistion nécessaire de cette commande.`);
 
     let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
     if(!member)
-        return message.channel.send(`S\'il vous plaît` "<@" + message.author.id + `>, merci de mentionner un membre valide sur ce serveur`);
+        return message.channel.send(`S\'il vous plaît <@` + message.author.id + `>, merci de mentionner un membre valide sur ce serveur`);
 
     if(!member.kickable)
         return message.channel.send("Je ne ne peux pas kické cette utilisateur, ai-je la permissions nécessaire ? Suis-je assez haut ?");
