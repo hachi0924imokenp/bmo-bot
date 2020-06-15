91c993b95eee80356f2fd95d495c7021d3b70c74
Cleaner-Discord <36578194+Cleaner-Discord@users.noreply.github.com>
2020-06-15 22:09:06 +0200

    Update tempmute.js

--- commands/tempmute.js
+++ commands/tempmute.js
@@ -1,5 +1,4 @@
 const ms = require("ms");
-
 exports.run = async (client, message, args) => {
   let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
   if(!tomute) return message.reply("Je ne peux pas trouver cette utilisateur.");
