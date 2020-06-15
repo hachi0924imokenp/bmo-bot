17923c855953b8f99045a875e6e97eead399a65a
Cleaner-Discord <36578194+Cleaner-Discord@users.noreply.github.com>
2020-06-15 14:48:34 +0200

    Update pm.js

--- commands/pm.js
+++ commands/pm.js
@@ -5,7 +5,7 @@ exports.run = async (client, message, args) => {
     let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
     if(!member)
         return message.channel.send(`S\'il vous plaît <@` + message.author.id + `>, merci de mentionner un membre valide sur ce serveur`).then(msg => {
-         msg.delete(10000)
+         message.delete({ timeout: 10000})
     }).catch(error => message.send(`Désolé ${message.author}, L'erreur suivante s'est produite durant l'exécution : ${error}`));
         
      let reason = args.slice(1).join(' ');
