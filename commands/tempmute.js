e25a187ec9ab252b62e6bdb73d3f5f67f0a0890f
Cleaner-Discord <36578194+Cleaner-Discord@users.noreply.github.com>
2020-06-16 00:27:13 +0200

    Update tempmute.js

--- commands/tempmute.js
+++ commands/tempmute.js
@@ -32,7 +32,10 @@ exports.run = async (client, message, args) => {
         if(!reason) reason = "Vous avez commis une infraction, un modérateurs vous a donc envoyé(e) en prison";
   
   await(tomute.roles.add(muterole.id));
-  client.users.cache.get(`616407988504363029`).send(`<@${tomute.id}> a été mis en prison par ${message.author.tag}`);
+  
+  const channel = client.channels.cache.get(`616407988504363029`);
+  channel.send(`<@${tomute.id}> a été mis en prison par ${message.author.tag}`);
+  
   client.users.cache.get(tomute);
     tomute.send(`${message.author.tag} t'envoie en prison ${ms(ms(mutetime))} ===> ${reason}`)
   
