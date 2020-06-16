4ce995ea7877db8fc48a1d14e8246483a44707bf
Cleaner-Discord <36578194+Cleaner-Discord@users.noreply.github.com>
2020-06-16 20:37:59 +0200

    Update poll.js

--- commands/poll.js
+++ commands/poll.js
@@ -3,4 +3,4 @@ exports.run = async (client, message, args, msg, title, options, timeout = 30, e
 	if(!message.member.roles.cache.some(r=>["🐹 Modo T'chat Test 🐹", "🛡️ P'tit Modo 🛡️", "🌟 Modo T'chat  🌟", "👑 Fondateurs 👑", "👑 Fondateur Principal 👑"].includes(r.name)))
     		return message.channel.send(`Désolé <@` + message.author.id + `>, vous n'avez pas la permission à l'utilistion nécessaire de cette commande.`);
 
-
+}
