b0fccda028199b31898805a82638a694d2a30df8
Cleaner-Discord <36578194+Cleaner-Discord@users.noreply.github.com>
2020-06-17 23:40:04 +0200

    Add files via upload

--- commands/pm.js
+++ commands/pm.js
@@ -3,7 +3,7 @@ exports.run = async (client, message, args) => {
     if(!message.member.roles.cache.some(r=>["🐹 Modo T'chat Test 🐹", "🛡️ P'tit Modo 🛡️", "🌟 Modo T'chat  🌟", "👑 Fondateurs 👑", "👑 Fondateur Principal 👑"].includes(r.name)))
         return message.channel.send(`Désolé <@` + message.author.id + `>, vous n'avez pas la permission à l'utilistion nécessaire de cette commande.`);
 
-        let member = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+    let member = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
         if(!member) return message.channel.send("Merci de mentionner un utilisateur sous la forme suivante:\n\nMention : ``@user#1234``\nDiscord ID : ``251455597738721280``");
       
     if (member.roles.cache.some(r=>["🐹 Modo T'chat Test 🐹", "🛡️ P'tit Modo 🛡️", "🌟 Modo T'chat  🌟", "👑 Fondateurs 👑", "👑 Fondateur Principal 👑"].includes(r.name)))
@@ -16,7 +16,7 @@ exports.run = async (client, message, args) => {
         if(!reason) reason = "Attention ! Tu as eu un comportement incorrecte, si cela se reproduit tu seras sanctionné !";
 
     const target = message.mentions.members.first() ||  message.guild.members.cache.get(args[0]);
-    client.users.cache.get(target);
-    message.author.send(`DM Envoyé à ${member.user.tag} !`).catch(() => message.channel.send(`DM Envoyé à ${member.user.tag} !`));
-    target.send(`MODO ${message.author.tag} ===> ${reason}`).catch(() => message.reply("Je n'ai pas plus envoyer de DM à votre utilisateur !"));
+        client.users.cache.get(target);
+        message.author.send(`DM Envoyé à ${member.user.tag} !`).catch(() => message.channel.send(`DM Envoyé à ${member.user.tag} !`));
+        target.send(`MODO ${message.author.tag} ===> ${reason}`).catch(() => message.reply("Je n'ai pas plus envoyer de DM à votre utilisateur !"));
 }
