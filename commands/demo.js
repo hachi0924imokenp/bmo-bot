a6cf08204eec784306da77ea82ba3ee36561e4ec
Cleaner-Discord <36578194+Cleaner-Discord@users.noreply.github.com>
2020-06-17 15:06:43 +0200

    Delete demo.js

--- commands/demo.js
+++
@@ -1,7 +0,0 @@
-exports.run = (client, message, args) => {
-
-    if(!message.author.id !== 251455597738721280) {
-        message.channel.send("Vous n'êtes pas mon créateur !");
-    } // Si cette personne n'a pas l'user id : 251455597738721280 il va envoyer un message et ne pas éxecuter la commande
-
-}
