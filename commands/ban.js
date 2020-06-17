96b07177b7a0774b48fe13aa99fde3dda262f7d5
Cleaner-Discord <36578194+Cleaner-Discord@users.noreply.github.com>
2020-06-17 04:18:20 +0200

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -23,8 +23,8 @@ exports.run = async (client, message, args) => {
     await member.ban(reason)
         .catch(error => message.channel.send(`Désolé, je ne peux pas bannir cette utilisateur à cause de : ${error}`));
   
-  const channel = client.users.cache.get(`616407988504363029`);
-  channel.send(`${member.user.tag} a été bannis par ${message.author.tag}`);
+  const target = client.users.cache.get(`616407988504363029`);
+  target.send(`${member.user.tag} a été bannis par ${message.author.tag}`);
 
   const logs = message.guild.channel.find.cache("𝐦𝐨𝐝-𝐥𝐨𝐠𝐬");
   if (message.guild.me.hasPermission('MANAGE_CHANNELS') && !logs) {
