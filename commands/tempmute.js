ee7fa6adffaf7c22bb6c26657a9799375e84c1ee
Cleaner-Discord <36578194+Cleaner-Discord@users.noreply.github.com>
2020-06-17 03:32:25 +0200

    Update tempmute.js

--- commands/tempmute.js
+++ commands/tempmute.js
@@ -3,7 +3,7 @@ const ms = require("ms");
 exports.run = async (client, message, args) => {
   
 let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.cache.get(args[0]));
-  if(!tomute) return message.channel.send("Merci de mentionner un utilisateur sous la forme suivante:\nMention : ``@user#1234``\nDiscord ID : ``251455597738721280``");
+  if(!tomute) return message.channel.send("Merci de mentionner un utilisateur sous la forme suivante:\n\nMention : ``@user#1234``\nDiscord ID : ``251455597738721280``");
   if(tomute.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Impossible d'envoyer cette utilisateur en prison !");
   
   let muterole =  message.guild.roles.cache.find(r => ["🏝️ No Man's Land"].includes(r.name));
