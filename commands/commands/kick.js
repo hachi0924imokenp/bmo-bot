b3217f8b3cef66398ea9b5c4c5811ca95c0fb5f9
Cleaner-Discord <36578194+Cleaner-Discord@users.noreply.github.com>
2020-06-17 23:46:59 +0200

    Add files via upload

---
+++ commands/commands/kick.js
@@ -0,0 +1,34 @@
+exports.run = async (client, message, args) => {
+    
+    if(!message.member.roles.cache.some(r=>["🐹 Modo T'chat Test 🐹", "🛡️ P'tit Modo 🛡️", "🌟 Modo T'chat  🌟", "👑 Fondateurs 👑", "👑 Fondateur Principal 👑"].includes(r.name)))
+        return message.channel.send(`Désolé <@` + message.author.id + `>, vous n'avez pas la permission nécessaire à l'utilistion  de cette commande.`);
+
+    let member = message.guild.member(message.mentions.users.first() || message.guild.members.cache.get(args[0]));
+    if(!member) return message.channel.send("Merci de mentionner un utilisateur sous la forme suivante:\n\nMention : ``@user#1234``\nDiscord ID : ``251455597738721280``");
+    
+    if (member.id !== bot.id);
+        message.channel.send("Impossible de m'envoyer en prison !");
+
+    if (member.user.bot)
+        return message.channel.send("Impossible d'avertir un bot !");
+
+    if (member.roles.cache.some(r=>["🐹 Modo T'chat Test 🐹", "🛡️ P'tit Modo 🛡️", "🌟 Modo T'chat  🌟", "👑 Fondateurs 👑", "👑 Fondateur Principal 👑"].includes(r.name)))
+        return message.channel.send("Impossible de kické un modérateur !");
+    
+    if(!member.kickable)
+        return message.channel.send("Je ne ne peux pas kické cette utilisateur, ai-je la permissions nécessaire ? Suis-je assez haut ?");
+
+    let reason = args.slice(1).join(' ');
+        if(!reason) reason = "Tu as commis une infraction, un modérateur t'a donc kické";
+    
+    const kicked = message.mentions.members.first() ||  message.guild.members.cache.get(args[0]);
+        client.users.cache.get(kicked);
+        kicked.send(`Tu as été kické par ${message.author.tag} ===> ${reason}`);
+    
+    await member.kick(reason)
+        .catch(error => message.channel.send(`Désolé, je ne peux pas kické cette utilisateur à cause de : ${error}`));
+   
+    const channel = client.channels.cache.get("616407988504363029");
+        channel.send(`${member.user.tag} a été kické par ${message.author.tag}`);
+
+}
---
+++ commands/commands/tempmute.js
@@ -0,0 +1,57 @@
+const ms = require("ms");
+exports.run = async (client, message, args) => {
+    if(!message.member.roles.cache.some(r=>["🐹 Modo T'chat Test 🐹", "🛡️ P'tit Modo 🛡️", "🌟 Modo T'chat  🌟", "👑 Fondateurs 👑", "👑 Fondateur Principal 👑"].includes(r.name)))
+        return message.channel.send(`Désolé <@` + message.author.id + `>, vous n'avez pas la permission nécessaire à l'utilistion  de cette commande.`);
+
+    let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.cache.get(args[0]));
+    if(!tomute) return message.channel.send("Merci de mentionner un utilisateur sous la forme suivante:\n\nMention : ``@user#1234``\nDiscord ID : ``251455597738721280``");
+
+    if (tomute.id !== bot.id);
+        message.channel.send("Impossible de m'envoyer en prison !");
+
+    if (member.user.bot)
+        return message.channel.send("Impossible d'avertir un bot !");
+
+    if (tomute.roles.cache.some(r=>["🐹 Modo T'chat Test 🐹", "🛡️ P'tit Modo 🛡️", "🌟 Modo T'chat  🌟", "👑 Fondateurs 👑", "👑 Fondateur Principal 👑"].includes(r.name)))
+        return message.channel.send("Impossible d'envoyer un modérateur en prison !");
+
+    let muterole =  message.guild.roles.cache.find(r => ["🏝️ No Man's Land"].includes(r.name));
+      if(!muterole){
+          try{
+            muterole = await message.guild.createRole({
+            name: "🏝️ No Man's Land",
+            color: "#ffd9000",
+            permissions:[]
+    })
+
+    message.guild.channels.forEach(async (channel, id ) =>{
+      await channel.overwritePermissions(muterole, {
+          SEND_MESSAGES: false,
+          ADD_REACTIONS: false
+        });
+      });
+    }catch(e){
+      console.log(e.stack);
+
+    }
+  }
+
+    let mutetime = args[1];
+        if(!mutetime) return message.reply("Vous n'avez pas spécifié le temps !");
+  
+    let reason = args.slice(2).join(' ');
+        if(!reason) reason = "Tu as commis une infraction, un modérateur t'a donc envoyé(e) en prison";
+  
+    await(tomute.roles.add(muterole.id));
+  
+    const channel = client.channels.cache.get("616407988504363029");
+        channel.send(`<${tomute.id}> a été mis en prison par ${message.author.tag}`);
+  
+    client.users.cache.get(tomute);
+        tomute.send(`${message.author.tag} t'envoie en prison ${ms(ms(mutetime))} ===> ${reason}`)
+  
+  
+    setTimeout(function(){
+        tomute.roles.remove(muterole.id);
+    }, ms(mutetime));
+}
