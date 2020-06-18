3707b4116af814ff8c1c78cb032ba7fabc02dc8d
Cleaner-Discord <36578194+Cleaner-Discord@users.noreply.github.com>
2020-06-18 16:42:45 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = (client) => {
   return () => {
     console.log(`-----------------------\nBMO est prêt à fonctionné !`);
-    client.user.setActivity(`my master !`, {type: "WATCHING"});
+    client.user.setActivity(`ma série favorite !`, {type: "LISTENING"});
   }
 }
