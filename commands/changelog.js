1f872ea551c95d48ee1a307028708bc18c187d26
Cleaner-Discord <36578194+Cleaner-Discord@users.noreply.github.com>
2020-06-18 03:18:03 +0200

    Add files via upload

--- commands/changelog.js
+++ commands/changelog.js
@@ -14,24 +14,41 @@ exports.run = async (client, message, args) => {
     title: "Changelog",
     url: "https://github.com/Cleaner-Discord/bmo-bot/commits/masterhttps://github.com/Cleaner-Discord/bmo-bot/commits/master",
     description: "Suivez ici les mises à jour de mon chère créateur ! <:BMOhappy:701199670466052246>",
+    thumbnail: {
+        url: 'https://i.imgur.com/wSTFkRM.png',
+    },
     fields: [{
         name: "Mise à jour du 18/06/20",
         value: "On a ajouter le changelog, c'est plus pratique non ? pour suivre les mises à jour du bot sans passer par Github <:BMOhappy:701199670466052246>"
+      },
+      {
+        name: '\u200b',
+        value: '\u200b',
+        inline: false,
       },
       {
         name: "Mise à jour du 17/06/20",
         value: "Correctif des problèmes au niveau des arguments sur certaines commandes ! \n\nCertains textes de réponses on été changer car c'était pas beau à voir <:BMOsad:699766556879618078>"
       },
       {
+        name: '\u200b',
+        value: '\u200b',
+        inline: false,
+      },
+      {
         name: "Dernière mise à jour effectué il y'a",
         value: `${duration}`
       },
+      
       {
         name: "Vous voulez voir un peu plus mes mise à jour ?",
         value: "Vous pouvez suivre mon avancement sur mon [Github](https://github.com/Cleaner-Discord/bmo-bot)."
       }
       
     ],
+    image: {
+        url: 'https://i.imgur.com/wSTFkRM.png',
+    },
     timestamp: new Date(),
     footer: {
       icon_url: "https://cdn.discordapp.com/avatars/548209665092091904/0a0054900dc4827350258c01ffc08470.png?size=128",
