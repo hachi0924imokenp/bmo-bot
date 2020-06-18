d84538b0ec0367d6a7772ae0c237d4b5759bd9e7
Cleaner-Discord <36578194+Cleaner-Discord@users.noreply.github.com>
2020-06-18 17:31:18 +0200

    Add files via upload

--- commands/warn.js
+++ commands/warn.js
@@ -3,177 +3,53 @@ const fs = require("fs");
 const ms = require("ms");
 let warns = JSON.parse(fs.readFileSync("./modules/warnings.json", "utf8"));
 
-exports.run = async (client, message, args) => {
-    message.delete(message.author);
+exports.run = (client, message, args) => {
 
-    if(!message.member.roles.cache.some(r=>["🐹 Modo T'chat Test 🐹", "🌟 Modo T'chat  🌟", "👑 Fondateurs 👑", "👑 Fondateur Principal 👑"].includes(r.name)))
-        return message.channel.send(`Désolé`+"<@" + message.author.id + `>, vous n'avez pas la permission nécessaire à l'utilistion de cette commande.`);
-
-    let member = message.guild.member(message.mentions.users.first() || message.guild.members.cache.get(args[0]));
-        if(!member) return message.channel.send("Merci de mentionner un utilisateur sous la forme suivante:\n\nMention : ``@user#1234``\nDiscord ID : ``251455597738721280``");
-    
-    if (member.user.bot)
-        return message.channel.send("Impossible de warn un bot !");
-
-    if(member.roles.cache.some(r=>["🐹 Modo T'chat Test 🐹", "🛡️ P'tit Modo 🛡️", "🌟 Modo T'chat  🌟", "👑 Fondateurs 👑", "👑 Fondateur Principal 👑"].includes(r.name)))
-        return message.channel.send("Impossible de warn un modérateur !");
-
-    let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.cache.get(args[0])
-        if(!wUser) return message.channel.send("Merci de mentionner un utilisateur sous la forme suivante:\n\nMention : ``@user#1234``\nDiscord ID : ``251455597738721280``");
-    
+    if(!message.member.hasPermission("MANAGE_MEMBERS")) return message.reply("Vous n'avez pas la permission nécessaire à l'utilisation de cette commande.");
+    let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
+    if(!wUser) return message.reply("Merci de mentionner un utilisateur sous la forme suivante:\n\nMention : ``@user#1234``\nDiscord ID : ``251455597738721280``");
+    if(wUser.hasPermission("MANAGE_MESSAGES")) return message.reply("Impossible de warn un modérateur !");
     let reason = args.join(" ").slice(22);
-        if(!reason) reason = "Tu as commis une infraction, un modérateur t'a donc Warn";
-
-        if(!warns[wUser.id]) warns[wUser.id] = {
-            warns: 0
-          };
-        
-        warns[wUser.id].warns++;
-
+  
+    if(!warns[wUser.id]) warns[wUser.id] = {
+      warns: 0
+    };
+  
+    warns[wUser.id].warns++;
+  
     fs.writeFile("./modules/warnings.json", JSON.stringify(warns), (err) => {
-            if (err) console.log(err)
+      if (err) console.log(err)
     });
-
-    const info = message.guild.channels.cache.find(c=>["informations"].includes(c.name))
-        setTimeout(function () {
-            if (message.guild.me.hasPermission('MANAGE_CHANNELS') && !info) {
-                message.guild.channels.create('informations').catch(error => message.channel.send(`Une erreur s'est produite durant la création du salon \"informations\" : ${error}`));
-        
-            }   
-        }, 2000);
-    
-            if (!message.guild.me.hasPermission('MANAGE_CHANNELS') && !info) { 
-                console.log('Le salon des informations n\'existe pas, et j\'ai essayer de le crée mais je manque de permissions !')
-            }
-    
-    const logchan = message.guild.channels.cache.find(c=>["𝐦𝐨𝐝-𝐥𝐨𝐠𝐬"].includes(c.name))
-        setTimeout(function () {
-            if (message.guild.me.hasPermission('MANAGE_CHANNELS') && !logchan) {
-                message.guild.channels.create('𝐦𝐨𝐝-𝐥𝐨𝐠𝐬').catch(error => message.channel.send(`Une erreur s'est produite durant la création du salon \"𝐦𝐨𝐝-𝐥𝐨𝐠𝐬\" : ${error}`));
-      
-            }
-        }, 2000);
-    
-            if (!message.guild.me.hasPermission('MANAGE_CHANNELS') && !logchan) { 
-                console.log('Le salon des logs n\'existe pas, et j\'ai essayer de le crée mais je manque de permissions !')
-            }   
-        
-    info.send(`${member.user.tag} a été warn par ${message.author.tag}`);
-    logchan.send({embed: {
-            color: '#fc0703',
-            author: {
-            name: member.user.tag,
-            icon_url: "https://cdn.discordapp.com/avatars/"+member.user.id+ "/"+member.user.avatar+".png"
-            },
-            title: "Warn",
-            description: "C'est la réputation qui en prends un coup !",
-            thumbnail: {
-                url:"https://cdn.discordapp.com/avatars/"+message.author.id+ "/"+message.author.avatar+".png",
-            },
-            fields: [{
-                name: "Action",
-                value: `Warn`,
-                inline: false,
-            },
-            {
-                name: "Nom d'utilisateur",
-                value: `${member.user.tag}`,
-                inline: false,
-            },
-            {
-                name: "ID",
-                value: `${member.user.id}`,
-                inline: false,
-            },
-            {
-                name: "Warn par",
-                value: `${message.author.tag}`,
-                inline: false,
-            },
-            {
-                name: "ID du Modérateur",
-                value: `${message.author.id}`,
-                inline: false,
-            },
-            {
-                name: "Raison",
-                value: `${reason}`,
-                inline: false,
-            }
-        ],
-            timestamp: new Date(),
-            footer: {
-            icon_url: "https://cdn.discordapp.com/avatars/548209665092091904/0a0054900dc4827350258c01ffc08470.png?size=128",
-            text: "© BMO"
-            }
-        }
-        }); 
-
+  
+    let warnEmbed = new Discord.RichEmbed()
+    .setDescription("Warn !")
+    .setAuthor(message.author.username)
+    .setColor("#fc6400")
+    .addField("Utilisateur Warn", `<@${wUser.id}>`)
+    .addField("Warn dans le salon", message.channel)
+    .addField("Nombre de warns", warns[wUser.id].warns)
+    .addField("Raison", reason);
+  
+    let warnchannel = message.guild.channels.cache.find(c=>["𝐦𝐨𝐝-𝐥𝐨𝐠𝐬"].includes(c.name));
+    if(!warnchannel) return message.reply("Je ne peux pas trouver le salon \'𝐦𝐨𝐝-𝐥𝐨𝐠𝐬\'");
+  
+    warnchannel.send(warnEmbed);
+  
     if(warns[wUser.id].warns == 4){
-    let muterole =  message.guild.roles.cache.find(r => ["🏝️ No Man's Land"].includes(r.name));
-    if(!muterole) return message.reply("Impossible de trouver le rôle '🏝️ No Man's Land', vous devez le crée !");
-    
-    let mutetime = "1h";
-        await(wUser.roles.add(muterole.id));
-        message.channel.send(`<@${wUser.id}> a été temporairement mute `);
-        
-    setTimeout(function(){
+    message.guild.roles.cache.find(r => ["🏝️ No Man's Land"].includes(r.name));
+    if(!muterole) return message.reply("Merci de crée un rôle nommée \'🏝️ No Man's Land\'");
+  
+      let mutetime = "5m";
+      await(wUser.addRole(muterole.id));
+      message.channel.send(`<@${wUser.id}> a été muté !`);
+  
+      setTimeout(function(){
         wUser.removeRole(muterole.id)
-        message.channel.send(`<@${wUser.id}> a été unmute`)
-        }, ms(mutetime))
+        message.reply(`<@${wUser.id}> a été unmute !`)
+      }, ms(mutetime))
     }
-
     if(warns[wUser.id].warns == 10){
-        message.guild.member(wUser).ban(reason);
-        message.channel.send(`<@${wUser.id}> a été bannis`)
-        logchan.send({embed: {
-            color: '#fc0703',
-            author: {
-            name: member.user.tag,
-            icon_url: "https://cdn.discordapp.com/avatars/"+member.user.id+ "/"+member.user.avatar+".png"
-        },
-            title: "Bannissement",
-            description: "Le Ban Hammer est tombé !",
-            thumbnail: {
-                url:"https://cdn.discordapp.com/avatars/"+message.author.id+ "/"+message.author.avatar+".png",
-            },
-            fields: [{
-                name: "Action",
-                value: `Ban`,
-                inline: false,
-            },
-            {
-                name: "Nom d'utilisateur",
-                value: `${member.user.tag}`,
-                inline: false,
-            },
-            {
-                name: "ID",
-                value: `${member.user.id}`,
-                inline: false,
-            },
-            {
-                name: "Bannis par",
-                value: `${message.author.tag}`,
-                inline: false,
-            },
-            {
-                name: "ID du Modérateur",
-                value: `${message.author.id}`,
-                inline: false,
-            },
-            {
-                name: "Raison",
-                value: `${reason}`,
-                inline: false,
-            }
-        ],
-            timestamp: new Date(),
-            footer: {
-            icon_url: "https://cdn.discordapp.com/avatars/548209665092091904/0a0054900dc4827350258c01ffc08470.png?size=128",
-            text: "© BMO"
-            }
-        }
-        });
-    }
+      message.guild.member(wUser).ban(reason);
+      message.reply(`<@${wUser.id}> a été bannis`)
+    }  
 }
---
+++ commands/warns.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+const ms = require("ms");
+let warns = JSON.parse(fs.readFileSync("./modules/warnings.json", "utf8"));
+
+module.exports.run = async (bot, message, args) => {
+
+  if(!warns[wUser.id]) warns[wUser.id] = {
+    warns: 0
+  };
+  
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Vous n'avez pas la permission nécessaire à l'utilisation de cette commande.");
+  let wUser = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
+  if(!wUser) return message.reply("Merci de mentionner un utilisateur sous la forme suivante:\n\nMention : ``@user#1234``\nDiscord ID : ``251455597738721280``");
+  let warnlevel = warns[wUser.id].warns;
+
+  message.reply(`<@${wUser.id}> a un total de : ${warnlevel} warns.`);
+
+}
