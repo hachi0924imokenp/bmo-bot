63716eeeff05955d549bcb9e0d86318db352e2b9
Cleaner-Discord <36578194+Cleaner-Discord@users.noreply.github.com>
2020-07-01 22:03:27 +0200

    Update message.js

--- events/message.js
+++ events/message.js
@@ -3,122 +3,116 @@ module.exports = (globalVariables) => {
     global[variable] = globalVariables[variable];
   });
 
-  async function event(message){
-
-    if (message.author.bot) return;
-
-    const swearWords = ["Fuck", "fuck"];
-    const insultron = new Discord.MessageEmbed()
-          .setTitle('Insultron')
-          .setColor("#ff0a0a")
-          .setThumbnail("https://cdn.discordapp.com/avatars/" + message.author.id + "/" + message.author.avatar + ".png")
-          .setAuthor(`${message.author.username}`, "https://cdn.discordapp.com/avatars/" + message.author.id + "/" + message.author.avatar + ".png")
-          .setDescription('Un mot suceptible d\'être innaproprié a été détecter, vous pouvez effectuer une action en utilisant les réactions ci-dessous !')
-          .addFields(
-            { name: '__**Action possibles**__', value: '\n🗑️ Détruire le message \n🛡️ Avertir l\'utilisateur en DM \n🔇 Mute (60 minutes) \n⚔️ Kick \n⛔ Bannir Définitivement \n❌ Ignorer',  inline: true},
-            { name: '\u200B', value: '\u200B',  inline: true},
-            { name: '\u200B', value: '__**Informations**__ :' },
-            { name: 'Pseudo :', value: message.author.username, inline: true },
-            { name: 'ID de l\'utilisateur :', value: message.author.id, inline: true },
-            { name: '\u200B', value: '\u200B', inline: true },
-            { name: 'Discriminateur :', value: '#' + message.author.discriminator, inline: true },
-            { name: 'Mention :', value:`<@${message.author.id}>`, inline: true },
-            { name: '\u200B', value: '\u200B', inline: true },
-            { name: 'Dans le salon :', value:`<#${message.channel.id}>`, inline: true },
-            { name: 'ID du salon :', value: `${message.channel.id}`, inline: true },
-            { name: '\u200B', value: '\u200B', inline: true },
-            { name: 'Message suceptible d\'être innaproprié :', value: `**${message.content.substr(0)}**`, inline: true },
-            { name: '\u200B', value: `[Cliquer ici pour afficher le salon](https://discord.com/channels/`+message.guild.id+`/`+message.channel.id+` 'Lien du salon')`},
-          )
-          .setTimestamp()
-          .setFooter('© BMO', "https://cdn.discordapp.com/avatars/" +client.user.id + "/" + client.user.avatar + ".png");
-            
-      if(swearWords.map(n => message.content.includes(n)).filter(n => n !== false)[0]){
-        if (message.member.roles.cache.some(r => ["🐹 Modo T'chat Test 🐹", "🛡️ P'tit Modo 🛡️", "🌟 Modo T'chat  🌟", "👑 Fondateurs 👑", "👑 Fondateur Principal 👑"].includes(r.name))) return;
-      
-        const cmd = message.guild.channels.cache.find(c => ["mod-cmds"].includes(c.name))
-          if (message.guild.me.hasPermission('MANAGE_CHANNELS') && !cmd) {
-            message.guild.channels.create('mod-cmds').then(async message => {
-            const sucess = new Discord.MessageEmbed()
-            .setTitle('Succès')
-            .setColor(')#81ff75')
-            .setDescription(`✅ Le salon \`\`\`mod-cmds\`\`\` a été crée avec succès`)
-            .setFooter('© BMO', client.user.avatarURL)
-            .setTimestamp();
-            message.channel.send('Le salon \'mod-cmds\' a été crée avec succès !')
-          })
-        } 
-          
-          if (!message.guild.me.hasPermission('MANAGE_CHANNELS') && !cmd) {
-            const error = new Discord.MessageEmbed()
-              .setTitle('Erreur')
-              .setColor('#FF0000')
-              .setDescription(`👨‍🔧 Une erreur s'est produite lors de la création du salon "mod-cmds"`)
-              .addField('Erreur :', '\`MISSING PERMISSION \'MANAGE_CHANNELS\'\`', false)
-              .setFooter('© BMO', client.user.avatarURL)
-              .setTimestamp();
-              
-              message.channel.send(error) 
-              return;
-          }   
-
-            cmd.send(insultron).then(async message => {
-              message.react("🗑️")
-              message.react("🛡️")
-              message.react("🔇")
-              message.react("⚔️")
-              message.react("⛔")
-              message.react("❌")
-      
-            const collector = message.createReactionCollector((reaction, user, client) => 
-                user.id === (!client.id) &&
-                reaction.emoji.name === "🗑️" ||
-                reaction.emoji.name === "🛡️" ||
-                reaction.emoji.name === "🔇" ||
-                reaction.emoji.name === "⚔️" ||
-                reaction.emoji.name === "⛔" ||
-                reaction.emoji.name === "❌"
-              ).once("collect", reaction => {
-                const chosen = reaction.emoji.name;
-                if(chosen === "🗑️"){
-                  message.edit("test1")
-                }
-                else if(chosen === "🛡️"){
-                  message.edit("test2")
-                }
-                else if(chosen === "🔇"){
-                  message.edit("test3")
-                }
-                else if(chosen === "⚔️"){
-                  message.edit("test4")
-                }
-                else if(chosen === "⛔"){
-                  message.edit("test5")
-                } else {
-                  message.edit("test6")
-                }
-                collector.stop();
-              })
-            })
-    }
-         
-    if (message.content.indexOf(prefix) !== 0) return;
-
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
-    let commands = getFiles(__dirname+"/../commands").filter(f => f.endsWith(".js"));
-   
-    if (message.channel.type ==="dm" || message.channel.type==="group")
-      return message.channel.send("Je ne suis pas fait pour fonctionner en DM/Groupes");
-
-    for(let i=0; i<commands.length; i++){
-      let command = require(commands[i])(globalVariables);
-      for(let n=0; n<command.options.name.length; n++){
-        if(message.content.toLowerCase().startsWith(prefix+command.options.name[n].toLowerCase()) && command.options.enable) return command(message, message.content.slice(prefix.length).trim().split(/ +/g));
+  async function command(message, args){
+    message.delete(message.author);
+    if (!message.member.roles.cache.some(r => ["🐹 Modo T'chat Test 🐹", "🛡️ P'tit Modo 🛡️", "🌟 Modo T'chat  🌟", "👑 Fondateurs 👑", "👑 Fondateur Principal 👑"].includes(r.name))) return message.channel.send(`Désolé <@` + message.author.id + `>, vous n'avez pas la permission nécessaire à l'utilistion  de cette commande.`);
+    let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.cache.get(args[0]));
+    if (!tomute) return message.channel.send("Merci de mentionner un utilisateur sous la forme suivante:\n\nMention : ``@user#1234``\nDiscord ID : ``251455597738721280``");
+    if(tomute.id === client.user.id) return message.channel.send("Hahaha, bien essayer mais je ne peux pas m\'envoyer en prison !");
+    if (tomute.user.bot) return message.channel.send("Impossible d'envoyer un bot en prison !");
+    if(tomute.id === message.author.id) return message.channel.send("Vous ne pouvez pas vous envoyer en prison vous-même");
+    if (tomute.roles.cache.some(r => ["🐹 Modo T'chat Test 🐹", "🛡️ P'tit Modo 🛡️", "🌟 Modo T'chat  🌟", "👑 Fondateurs 👑", "👑 Fondateur Principal 👑"].includes(r.name))) return message.channel.send("Impossible d'envoyer un modérateur en prison !");
+    let muterole = message.guild.roles.cache.find(r => ["🏝️ No Man's Land"].includes(r.name));
+    if (!muterole) {
+      try {
+        muterole = await message.guild.createRole({
+          name: "🏝️ No Man's Land",
+          color: "#ffd9000",
+          permissions: []
+        })
+        message.guild.channels.forEach(async (channel, id) => {
+          await channel.overwritePermissions(muterole, {
+            SEND_MESSAGES: false,
+            ADD_REACTIONS: false
+          });
+        });
+      } catch (e) {
+        console.log(e.stack);
       }
     }
+    let mutetime = args[1];
+    if (!mutetime) return message.reply("Vous n'avez pas spécifié le temps !");
+    let reason = args.slice(2).join(' ');
+    if (!reason) reason = "Tu as commis une infraction, un modérateur t'a donc envoyé(e) en prison";
+    await (tomute.roles.add(muterole.id));
+    client.users.cache.get(tomute);
+    tomute.send(`${message.author.tag} t'envoie en prison ${ms(ms(mutetime))} ===> ${reason}`)
+    const info = message.guild.channels.cache.find(c => ["informations"].includes(c.name))
+    setTimeout(function() {
+      if (message.guild.me.hasPermission('MANAGE_CHANNELS') && !info) {
+        message.guild.channels.create('informations').catch(error => message.channel.send(`Une erreur s'est produite durant la création du salon \"informations\" : ${error}`));
+      }
+    }, 2000);
+    if (!message.guild.me.hasPermission('MANAGE_CHANNELS') && !info) {
+      console.log('Le salon des informations n\'existe pas, et j\'ai essayer de le crée mais je manque de permissions !')
+    }
+    const logchan = message.guild.channels.cache.find(c => ["𝐦𝐨𝐝-𝐥𝐨𝐠𝐬"].includes(c.name))
+    setTimeout(function() {
+      if (message.guild.me.hasPermission('MANAGE_CHANNELS') && !logchan) {
+        message.guild.channels.create('𝐦𝐨𝐝-𝐥𝐨𝐠𝐬').catch(error => message.channel.send(`Une erreur s'est produite durant la création du salon \"𝐦𝐨𝐝-𝐥𝐨𝐠𝐬\" : ${error}`));
+      }
+    }, 2000);
+    if (!message.guild.me.hasPermission('MANAGE_CHANNELS') && !logchan) {
+      console.log('Le salon des logs n\'existe pas, et j\'ai essayer de le crée mais je manque de permissions !')
+    }
+    info.send(`${tomute.user.tag} a été mis en prison par ${message.author.tag}`);
+    logchan.send({
+      embed: {
+        color: '#fc0703',
+        author: {
+          name: tomute.user.tag,
+          icon_url: "https://cdn.discordapp.com/avatars/" + tomute.user.id + "/" + tomute.user.avatar + ".png"
+        },
+        title: "Mute",
+        description: "Get Jailed B*tch :D",
+        thumbnail: {
+          url: "https://cdn.discordapp.com/avatars/" + message.author.id + "/" + message.author.avatar + ".png",
+        },
+        fields: [{
+          name: "Action",
+          value: `Mute`,
+          inline: false,
+        }, {
+          name: "Nom d'utilisateur",
+          value: `${tomute.user.tag}`,
+          inline: false,
+        }, {
+          name: "ID",
+          value: `${tomute.user.id}`,
+          inline: false,
+        }, {
+          name: "Muté par",
+          value: `${message.author.tag}`,
+          inline: false,
+        }, {
+          name: "ID du Modérateur",
+          value: `${message.author.id}`,
+          inline: false,
+        }, {
+          name: "Temps",
+          value: `${mutetime}`,
+          inline: false,
+        }, {
+          name: "Raison",
+          value: `${reason}`,
+          inline: false,
+        }],
+        timestamp: new Date(),
+        footer: {
+          icon_url: "https://cdn.discordapp.com/avatars/548209665092091904/0a0054900dc4827350258c01ffc08470.png?size=128",
+          text: "© BMO"
+        }
+      }
+    });
+    setTimeout(function() {
+      tomute.roles.remove(muterole.id);
+    }, ms(mutetime));
   }
 
-  event.listener = "message";
+  command.options = {
+    name: ["mute"],
+    enable: true
+  };
 
-  return event;
+  return command;
 }
