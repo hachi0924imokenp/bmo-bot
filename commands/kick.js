be7089b345e6938117a66974644a4d1896f23046
Cleaner-Discord <36578194+Cleaner-Discord@users.noreply.github.com>
2020-07-04 03:24:47 +0200

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -13,7 +13,7 @@ module.exports = (globalVariables) => {
     if(member.id === message.author.id) return message.channel.send("Vous ne pouvez pas vous kické vous-même");
     if (member.roles.cache.some(r => ["🐹 Modo T'chat Test 🐹", "🛡️ P'tit Modo 🛡️", "🌟 Modo T'chat  🌟", "👑 Fondateurs 👑", "👑 Fondateur Principal 👑"].includes(r.name))) return message.channel.send("Impossible de kické un modérateur !");
     if (!member.kickable) return message.channel.send("Je ne ne peux pas kické cette utilisateur, ai-je la permissions nécessaire ? Suis-je assez haut ?");
-    let reason = args.slice(1).join(' ');
+    let reason = args.slice(2).join(' ');
     if (!reason) reason = "Tu as commis une infraction, un modérateur t'a donc kické";
     const kicked = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
     client.users.cache.get(kicked);
